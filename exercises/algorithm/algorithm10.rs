f1522b610010b420bc5170926fddd4c4ab115db7
root <root@DESKTOP-JC39N5C>
2024-10-21 00:03:07 +0800

    123

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -6,43 +6,70 @@
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
+
 #[derive(Debug, Clone)]
-pub struct NodeNotInGraph;
+pub struct NodeNotInGraph; 
+
 impl fmt::Display for NodeNotInGraph {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "accessing a node that is not in the graph")
     }
 }
+
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
+
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
         UndirectedGraph {
             adjacency_table: HashMap::new(),
         }
     }
+
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
         &mut self.adjacency_table
     }
+
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
+
+    fn add_node(&mut self, node: &str) -> bool {
+        if !self.contains(node) {
+            self.adjacency_table.insert(String::from(node), Vec::new());
+            true
+        } else {
+            false
+        }
+    }
+
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (node1, node2, weight) = edge;
+
+        // Ensure both nodes exist in the graph before adding the edge
+        if !self.contains(node1) {
+            self.add_node(node1);
+        }
+        if !self.contains(node2) {
+            self.add_node(node2);
+        }
+
+        // Add the edge to both nodes
+        self.adjacency_table.entry(String::from(node1)).or_insert_with(Vec::new)
+            .push((String::from(node2), weight));
+
+        self.adjacency_table.entry(String::from(node2)).or_insert_with(Vec::new)
+            .push((String::from(node1), weight));
     }
 }
+
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
-    fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
-    }
-    fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
-    }
+    fn add_node(&mut self, node: &str) -> bool;
+    fn add_edge(&mut self, edge: (&str, &str, i32));
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
     }
@@ -59,26 +86,31 @@ pub trait Graph {
         edges
     }
 }
+
+#[cfg(test)]
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
     use super::UndirectedGraph;
+
     #[test]
     fn test_add_edge() {
         let mut graph = UndirectedGraph::new();
         graph.add_edge(("a", "b", 5));
         graph.add_edge(("b", "c", 10));
         graph.add_edge(("c", "a", 7));
-        let expected_edges = [
-            (&String::from("a"), &String::from("b"), 5),
-            (&String::from("b"), &String::from("a"), 5),
-            (&String::from("c"), &String::from("a"), 7),
-            (&String::from("a"), &String::from("c"), 7),
-            (&String::from("b"), &String::from("c"), 10),
-            (&String::from("c"), &String::from("b"), 10),
+
+        let expected_edges = vec![
+            (String::from("a"), String::from("b"), 5),
+            (String::from("b"), String::from("a"), 5),
+            (String::from("c"), String::from("a"), 7),
+            (String::from("a"), String::from("c"), 7),
+            (String::from("b"), String::from("c"), 10),
+            (String::from("c"), String::from("b"), 10),
         ];
-        for edge in expected_edges.iter() {
-            assert_eq!(graph.edges().contains(edge), true);
+
+        for edge in &expected_edges {
+            assert!(graph.edges().contains(edge));
         }
     }
 }
--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -20,6 +20,5 @@ fn main() {
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "rustc-cfg=pass";
-    println!("cargo:{}", your_command);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
